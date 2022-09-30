294507d1f48b0d63c34130d046b719a70f66860a
bvlucretiu <vasilebirta@gmail.com>
2022-09-30 11:34:27 +0300

    Edited for display a list

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Superman' },
+  { id: 13, name: 'Batman' },
+  { id: 14, name: 'Spiderman' },
+  { id: 15, name: 'Zorro' },
+  { id: 16, name: 'Piedone' },
+  { id: 17, name: 'Robocop' },
+  { id: 18, name: 'Picard' },
+  { id: 19, name: 'Bruce Lee' },
+  { id: 20, name: 'Tornado' },
+];
