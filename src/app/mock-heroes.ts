55147a94455a854feab827d8f35d0ca84681820e
bvlucretiu <vasilebirta@gmail.com>
2022-09-30 20:03:00 +0300

    Add new property to hero

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,13 +1,13 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 12, name: 'Superman' },
-  { id: 13, name: 'Batman' },
-  { id: 14, name: 'Spiderman' },
-  { id: 15, name: 'Zorro' },
-  { id: 16, name: 'Piedone' },
-  { id: 17, name: 'Robocop' },
-  { id: 18, name: 'Picard' },
-  { id: 19, name: 'Bruce Lee' },
-  { id: 20, name: 'Tornado' },
+  { id: 12, name: 'Superman', detail: 'fly' },
+  { id: 13, name: 'Batman', detail: 'work' },
+  { id: 14, name: 'Spiderman', detail: 'fly' },
+  { id: 15, name: 'Zorro', detail: 'masc' },
+  { id: 16, name: 'Piedone', detail: 'bold' },
+  { id: 17, name: 'Robocop', detail: 'ironCap' },
+  { id: 18, name: 'Picard', detail: 'captain' },
+  { id: 19, name: 'Bruce Lee', detail: 'kung-fu' },
+  { id: 20, name: 'Tornado', detail: 'fly' },
 ];
